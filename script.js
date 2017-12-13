47b60d8a9a24bcf5d774fe31f9b324d51f04cae1
CyberSonic60963240 <Gregory_Cowgill@davincischools.org>
2017-12-12 16:09:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function playMidi(e){
+	document.getElementById("MIDI").play("Green_Hill_Zone.MP3");
+	document.body.style.backgroundImage = "url('Sunek1.jpg')";
+}
+// ".play" plays the media to whom the Id belongs and that which is specified by the file url in parentheses and quotation marks upon meeting the stated quota specified in the linked HTML page (for example, if the "onclick" specification is made, then the media will play upon the user clicking the corresponding div(s)), while "body.style.backgroundImage" specifies the image that is displayed in the background of the page's body according to the url of the file that is in the following parentheses under the same HTML conditions as the ".play" code.
+function playChaos(e){
+	document.getElementById("Chaos").play("Open_Your_Heart.mp3");
+	document.body.style.backgroundImage = "url('Sunek_Etvuncher.jpg')";
+}
+
+function playSolaris(e){
+	document.getElementById("Solaris").play("His_World.mp3");
+	document.body.style.backgroundImage = "url('Sunek_06.JPG')";
+}
+
+function playZeti(e){
+	document.getElementById("Zeti").play("Wonder_World.mp3");
+	document.body.style.backgroundImage = "url('Sunek_List_Werd.jpg')";
+}
+
+function playInfinite(e){
+	document.getElementById("Infinite").play("Fist_Bump.oga");
+	document.body.style.backgroundImage = "url('Sunek_Furssess.jpg')";
+}
+//None of this JavaScript code is my own; I do not remember where I obtained it, but I think I got at least ome of it from W3Schools.com (https://www.w3schools.com/default.asp)
+//My ability to comment in JavaScript was also taken from W3Schools.com (https://www.w3schools.com/js/js_comments.asp).
